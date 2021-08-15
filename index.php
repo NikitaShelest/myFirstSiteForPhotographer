53652a48d98e32228db66ddf83e36dc8f6106393
NikitaShelest <shelestnikita1118@gmail.com>
2021-08-15 22:33:09 +0300

    First edit

--- index.php
+++ index.php
@@ -1,51 +1,49 @@
 <title>Главная</title>
 
-<?php 
-  require ('include/db.php');
+<?php
+require('include/db.php');
 
-  $issue = new db();
-  $instagram = $issue->issuingContact()[0]['instagram'];
-  $facebook = $issue->issuingContact()[0]['facebook'];
-  $vk = $issue->issuingContact()[0]['vk'];
-  $head = $issue->issuingAbout()[0]['head']; 
-  $opening = $issue->issuingAbout()[0]['opening'];
+$issue = new db();
+$instagram = $issue->issuingContact()[0]['instagram'];
+$facebook = $issue->issuingContact()[0]['facebook'];
+$vk = $issue->issuingContact()[0]['vk'];
+$head = $issue->issuingAbout()[0]['head'];
+$opening = $issue->issuingAbout()[0]['opening'];
 
-  $random_photo = $issue->issuingSlaiderImage();
+$random_photo = $issue->issuingSlaiderImage();
 
 ?>
-<?php 
-    include "static/header.php";
+<?php
+include "static/header.php";
 ?>
 
-    <?php include "static/changeDesign.php" ?>
+<?php include "static/changeDesign.php" ?>
 
-        <section class="main-content">
-                <div class="content">
-                    <h3><?php echo $head;?></h3>
-                </div>
-                <div class="content">
-                    <p><?php echo $opening;?></p>
-                </div>
-        </section>
+<section class="main-content">
+    <div class="content">
+        <h3><?php echo $head; ?></h3>
+    </div>
+    <div class="content">
+        <p><?php echo $opening; ?></p>
+    </div>
+</section>
 
-        <script src="JS/slider.js" defer></script>
-        <div class="container">
-            <div class="slider">
-                    <?php 
-                        foreach ($random_photo as $key => $id) {
-                            echo "<img class='hiddenPhoto' src='".$random_photo[$key]["link"]."' alt='photo' hidden>";
+<script src="JS/slider.js" defer></script>
+<div class="container">
+    <div class="slider">
+        <?php
+        foreach ($random_photo as $key => $id) {
+            echo "<img class='hiddenPhoto' src='" . $random_photo[$key]["link"] . "' alt='photo' hidden>";
+        }
+        ?>
+        <img class="slide" src="#" alt="img" width=400px>
+        <button class="button btn-left">❮|</button>
+        <button class="button btn-right">|❯</button>
+    </div>
+</div>
 
-                        }
-                    ?>
-                    <img class="slide" src="#" alt="img" width=400px>
-                    <button class="button btn-left">❮</button>
-                    <button class="button btn-right">❯</button>
-                </div>
-        </div>
-        
-        
-     
-<?php 
-    include "static/footer.php";
+
+
+<?php
+include "static/footer.php";
 ?>
-
